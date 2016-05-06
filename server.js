2d1dcc47e0d791a1f0c28cbb3e41d7e064113e4a
Sneha Vujjini <snehavujjini@gmail.com>
2016-05-05 19:29:01 -0700

    get todos

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [{
+	id: 1,
+	description: 'Homework',
+	completed: false
+}, {
+	id: 2,
+	description: 'finish lecture',
+	completed: false
+}, {
+	id: 3,
+	description: 'get a job',
+	completed: true
+}];
 
 app.get('/', function (req,res){
-	res.send('Todo API root');
+	res.send('Todo API Root');
+});
+
+app.get('/todos', function(req,res) {
+   res.json(todos);
 });
 
 app.listen(PORT, function () {
