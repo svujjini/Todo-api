2ebe3f0c0295209f65f27b6f07212169992f1506
Sneha Vujjini <snehavujjini@gmail.com>
2016-05-26 15:50:32 -0700

    changes to get

--- server.js
+++ server.js
@@ -15,35 +15,36 @@ app.get('/', function (req,res){
 });
 
 app.get('/todos', function(req,res) {
-   var queryParams = req.query;
-   var filteredTodos = todos;
+   var query = req.query;
+   var where = {};
+   //var filteredTodos = todos;
 
-   if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-   	filteredTodos = _.where(filteredTodos, {completed: true});
-   } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-   	filteredTodos = _.where(filteredTodos, {completed: false});
+   if(query.hasOwnProperty('completed') && query.completed === 'true') {
+   	where.completed = true;
+   } else if(query.hasOwnProperty('completed') && query.completed === 'false') {
+   	 where.completed = false;
    }
 
-   if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function (todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-		});
+   if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
+
 	}
 
-    res.json(filteredTodos);
+    db.todo.findAll({
+		where: where
+	}).then(function(todos) {
+		res.json(todos);
+	}, function(e) {
+		res.status(500).send();
+	});
+
 });
 
 app.get('/todos/:id', function(req,res) {
 	var todoId = parseInt(req.params.id,10);
-	// var matchedTodo = _.findWhere(todos, {id: todoId});
-
-	// if(matchedTodo) {
-	// 	res.json(matchedTodo);
-	// } else {
-	// 	res.status(404).send();
-	// }
-
- //     res.send('Asking for todo with id of ' + req.params.id);
+	
     db.todo.findById(todoId).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
@@ -59,17 +60,6 @@ app.get('/todos/:id', function(req,res) {
 //new todo
 app.post('/todos', function(req,res) {
    var body = _.pick(req.body, 'description','completed');
-
-   // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-   // 	return res.status(400).send();
-   // }
-
-   // body.description =body.description.trim();
-
-   // body.id = todoNextId++;
-   // todos.push(body);
-
-   // res.json(body);
 
    db.todo.create(body).then(function(todo) {
 		res.json(todo.toJSON());
