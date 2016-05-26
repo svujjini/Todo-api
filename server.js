c803c1924cae38fc37b891012a11fa3cf07cc1fb
Sneha Vujjini <snehavujjini@gmail.com>
2016-05-26 15:42:46 -0700

    changes to get/id

--- server.js
+++ server.js
@@ -35,15 +35,25 @@ app.get('/todos', function(req,res) {
 
 app.get('/todos/:id', function(req,res) {
 	var todoId = parseInt(req.params.id,10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	// var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	if(matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	// if(matchedTodo) {
+	// 	res.json(matchedTodo);
+	// } else {
+	// 	res.status(404).send();
+	// }
 
-     res.send('Asking for todo with id of ' + req.params.id);
+ //     res.send('Asking for todo with id of ' + req.params.id);
+    db.todo.findById(todoId).then(function(todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function(e) {
+		res.status(500).send();
+	});
+
 });
 
 //new todo
