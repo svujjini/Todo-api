244e45bce014c0a27e74fd4487a1e44b7af02dac
svujjini <snehavujjini@gmail.com>
2016-06-09 16:15:50 -0700

    changes to user login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcrypt = require('bcrypt');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -124,6 +125,28 @@ app.post('/users', function (req, res) {
 	}, function (e) {
 		res.status(400).json(e);
 	});
+});
+
+app.post('/users/login', function (req,res){
+    var body = _.pick(req.body, 'email', 'password');
+
+		if(typeof body.email !== 'string' || typeof body.password !== 'string'){
+			return res.status(400).send();
+		}
+
+		db.user.findOne({
+			where: {
+				email: body.email
+			}
+		}).then(function(user){
+			if(!user || !bcrypt.compareSync(body.password,user.get('password_hash'))) {
+				return res.status(401).send();
+			}
+
+			res.json(user.toPublicJSON());
+		},function (e) {
+			res.status(500).send();
+		});
 });
 
 db.sequelize.sync().then(function() {
