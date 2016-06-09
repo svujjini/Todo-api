36633dc2508fbdb66a94e1a263fab64fd1f43731
svujjini <snehavujjini@gmail.com>
2016-06-09 16:37:15 -0700

    modified  user login

--- server.js
+++ server.js
@@ -136,26 +136,9 @@ app.post('/users/login', function (req,res){
        res.status(401).send();
 		});
 
-		// if(typeof body.email !== 'string' || typeof body.password !== 'string'){
-		// 	return res.status(400).send();
-		// }
-		//
-		// db.user.findOne({
-		// 	where: {
-		// 		email: body.email
-		// 	}
-		// }).then(function(user){
-		// 	if(!user || !bcrypt.compareSync(body.password,user.get('password_hash'))) {
-		// 		return res.status(401).send();
-		// 	}
-		//
-		// 	res.json(user.toPublicJSON());
-		// },function (e) {
-		// 	res.status(500).send();
-		// });
-});
+	});
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force : true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
