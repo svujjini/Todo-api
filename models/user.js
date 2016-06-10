869f78f4730833b0cc2639692f54b8b889f5c573
svujjini <snehavujjini@gmail.com>
2016-06-09 17:53:22 -0700

    todos made private

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ var _ = require('underscore');
 var cryptojs = require('crypto-js');
 var jwt = require('jsonwebtoken');
 
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
 		email: {
 			type: DataTypes.STRING,
@@ -25,71 +25,96 @@ module.exports = function (sequelize, DataTypes) {
 			validate: {
 				len: [7, 100]
 			},
-			set: function(value){
+			set: function(value) {
 				var salt = bcrypt.genSaltSync(10);
-				var hashedPassword = bcrypt.hashSync(value,salt);
+				var hashedPassword = bcrypt.hashSync(value, salt);
 
 				this.setDataValue('password', value);
 				this.setDataValue('salt', salt);
 				this.setDataValue('password_hash', hashedPassword);
 			}
 		}
-	},
-	{
-			hooks: {
-				beforeValidate: function(user, options) {
-					 if (typeof user.email === 'string'){
-						 user.email = user.email.toLowerCase();
-					 }
+	}, {
+		hooks: {
+			beforeValidate: function(user, options) {
+				// user.email
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
 				}
-			},
-			classMethods: {
-				authenticate : function(body) {
-					return new Promise(function (resolve, reject) {
-						if(typeof body.email !== 'string' || typeof body.password !== 'string'){
+			}
+		},
+		classMethods: {
+			authenticate: function(body) {
+				return new Promise(function(resolve, reject) {
+					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+						return reject();
+					}
+
+					user.findOne({
+						where: {
+							email: body.email
+						}
+					}).then(function(user) {
+						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
 							return reject();
 						}
 
-						user.findOne({
-							where: {
-								email: body.email
-							}
-						}).then(function(user){
-							if(!user || !bcrypt.compareSync(body.password,user.get('password_hash'))) {
-								return reject();
-							}
+						resolve(user);
+					}, function(e) {
+						reject();
+					});
+				});
+			},
+			findByToken: function(token) {
+				return new Promise(function(resolve, reject) {
+					try {
+						var decodedJWT = jwt.verify(token, 'qwerty098');
+						var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#!');
+						var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
 
-							resolve(user);
-						},function (e) {
+						user.findById(tokenData.id).then(function (user) {
+							if (user) {
+								resolve(user);
+							} else {
+								reject();
+							}
+						}, function (e) {
 							reject();
 						});
-
-					});
-				}
+					} catch (e) {
+						reject();
+					}
+				});
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function() {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
 			},
-			instanceMethods: {
-				toPublicJSON: function() {
-					var json = this.toJSON();
-					return _.pick(json,'id','email','createdAt','updatedAt');
-				},
-				generateToken: function (type) {
-					if (!_.isString(type)) {
-						return undefined;
-					}
-					try{
-						var stringData = JSON.stringify({ id: this.get('id'), type: type});
-            var encryptedData = cryptojs.AES.encrypt(stringData, ' abc123!@#!').toString();
-						var token = jwt.sign({
-							token: encryptedData
-						}, 'qwerty123');
+			generateToken: function(type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
 
-						return token;
-					} catch (e){
-						 console.error(e);
-						 return undefined;
-					}
+				try {
+					var stringData = JSON.stringify({
+						id: this.get('id'),
+						type: type
+					});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc123!@#!').toString();
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'qwerty098');
+
+					return token;
+				} catch (e) {
+					console.error(e);
+					return undefined;
 				}
 			}
+		}
 	});
+
 	return user;
 };
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
+
 
 app.use(bodyParser.json());
 
@@ -16,7 +18,7 @@ app.get('/', function(req, res) {
 });
 
 // GET /todos?completed=false&q=work
-app.get('/todos', function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
 	var where = {};
 
@@ -42,7 +44,7 @@ app.get('/todos', function(req, res) {
 });
 
 // GET /todos/:id
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.findById(todoId).then(function(todo) {
@@ -57,7 +59,7 @@ app.get('/todos/:id', function(req, res) {
 });
 
 // POST /todos
-app.post('/todos', function(req, res) {
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function(todo) {
@@ -68,7 +70,7 @@ app.post('/todos', function(req, res) {
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.destroy({
@@ -89,7 +91,7 @@ app.delete('/todos/:id', function(req, res) {
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var body = _.pick(req.body, 'description', 'completed');
 	var attributes = {};
@@ -127,23 +129,24 @@ app.post('/users', function (req, res) {
 	});
 });
 
-app.post('/users/login', function (req,res){
-    var body = _.pick(req.body, 'email', 'password');
+// POST /users/login
+app.post('/users/login', function (req, res) {
+	var body = _.pick(req.body, 'email', 'password');
 
-		db.user.authenticate(body).then(function(user) {
-			var token = user.generateToken('authentication');
-			if (token) {
+	db.user.authenticate(body).then(function (user) {
+		var token = user.generateToken('authentication');
+
+		if (token) {
 			res.header('Auth', token).json(user.toPublicJSON());
 		} else {
 			res.status(401).send();
 		}
-		}, function() {
-       res.status(401).send();
-		});
-
+	}, function () {
+		res.status(401).send();
 	});
+});
 
-db.sequelize.sync({force : true}).then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
